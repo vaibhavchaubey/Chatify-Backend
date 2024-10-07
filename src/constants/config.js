fb9b578763fdd7fa28410bc92a9cb6978fa24ccb
Vaibhav <vaibhavchaubey786@gmail.com>
2024-10-08 00:55:16 +0530

    backend completed

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,7 +2,6 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'http://localhost:4173',
     process.env.CLIENT_URL,
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
